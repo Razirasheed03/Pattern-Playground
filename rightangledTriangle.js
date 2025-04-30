991a8b9af14364afeb73137c3b663e9cd0900272
Razirasheed03 <razirasheed03@gmail.com>
2025-04-30 22:20:31 +0530

    changes made

--- rightangledTriangle.js
+++ rightangledTriangle.js
@@ -1,8 +1,8 @@
 
-for(let i=1;i<=5;i++){
-    let stars=""
-    for(let j=1;j<=i;j++){
-        stars+="*"
+for (let i = 1; i <= 5; i++) {
+    let stars = ""
+    for (let j = 1; j <= i; j++) {
+        stars += "*"
     }
     console.log(stars)
 }
